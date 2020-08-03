5a8a5c65b158077d31616a7a2b1346912a0d5e06
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2020-08-03 19:57:04 +0200

    Add endpoint with json data

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Camera struct {
+	Name        string `json:"Name"`
+	URL         string `json:"URL"`
+	Description string `json:"Description"`
+}
+
+type Cameras []Camera
+
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home")
+	fmt.Fprintf(w, "<a href='/cameras'>Cameras</a>")
+}
+
+func allCameras(w http.ResponseWriter, r *http.Request) {
+	cameras :=
+		Cameras{
+			Camera{Name: "Morskie Oko", URL: "http://pogoda.topr.pl/download/current/mors.jpeg", Description: "Morskie Oko view"},
+			Camera{Name: "Morskie Oko", URL: "http://pogoda.topr.pl/download/current/mors.jpeg", Description: "Morskie Oko view"},
+			Camera{Name: "Morskie Oko", URL: "http://pogoda.topr.pl/download/current/mors.jpeg", Description: "Morskie Oko view"},
+		}
+	fmt.Println("All cameras")
+	json.NewEncoder(w).Encode(cameras)
 }
 
 func handleRequests() {
 	http.HandleFunc("/", home)
+	http.HandleFunc("/cameras", allCameras)
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
 
